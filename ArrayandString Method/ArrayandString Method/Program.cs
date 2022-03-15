251dbefb7f328357f71069f203a0396efa22de5b
Malik8008 <tu60prbs1@code.edu.az>
2022-03-15 07:38:08 +0400

    Replace completed

---
+++ ArrayandString Method/ArrayandString Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayandString_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "bU tasKI metHodla yAzmAliSiz";
+            string newsentence = sentence.Replace("bU tasKI metHodla yAzmAliSiz","Bu Taski Methodla Yazmalisiz");                        
+            Console.WriteLine(newsentence);
+        }
+    }
+}
